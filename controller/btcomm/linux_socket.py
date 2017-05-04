332b700bdf21569c648dc9fda1348dc39cc0ec27
Kristian Sims <kristian.sims@gmail.com>
2017-05-04 15:29:28 -0600

    added retry loop and help command

--- controller/btcomm/linux_socket.py
+++ controller/btcomm/linux_socket.py
@@ -3,16 +3,32 @@ import asyncio
 
 
 class BTComm:
-    def __init__(self, addr, loop):
+    def __init__(self, addr, loop, retries=3):
         self.addr = addr
         self.loop = loop
+        self.retries = retries
         self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
                                   socket.BTPROTO_RFCOMM)
         self.queue = asyncio.Queue(loop=loop)
 
     async def connect(self):
-        print('Connecting to {}'.format(self.addr))
-        self.sock.connect((self.addr, 1))
+        print('Bluetooth connecting to {}'.format(self.addr))
+        for n in range(self.retries, -1, -1):
+            try:
+                self.sock.connect((self.addr, 1))
+            except OSError:
+                print('Bluetooth connection failed.', end=' ')
+                if n > 0:
+                    print('Retrying... ({} remaining)'.format(n), flush=True)
+                    self.sock.close()
+                    self.sock = socket.socket(socket.AF_BLUETOOTH,
+                                              socket.SOCK_STREAM,
+                                              socket.BTPROTO_RFCOMM)
+                else:
+                    print('Aborting', flush=True)
+                    raise OSError
+            else:
+                break
         self.sock.setblocking(0)
         # selector = asyncio.selectors.DefaultSelector()
         # selector.register(self.sock, asyncio.selectors.EVENT_WRITE)
--- controller/server.py
+++ controller/server.py
@@ -11,13 +11,25 @@ class Server():
         self.loop = asyncio.get_event_loop()
         self.robot = Robot('MSE430-5', self.loop)
         self.server = None
+        self.commands = {
+            'objects': self.objects,
+            'robot': self.getrobot,
+            'obstacles': self.obstacles,
+            'setspeed': self.setspeed,
+            'setpower': self.setpower,
+            'setparam': self.setparam,
+            'help': self.help,
+            '?': self.help
+        }
         
     def run(self):
-        self.server = self.loop.run_until_complete(self.loop.create_server(
-            lambda: ServerProtocol(self), port=55555))
-        print('Serving on {}'.format(self.server.sockets[0].getsockname()))
         try:
             self.loop.run_until_complete(self.robot.connect())
+            self.server = self.loop.run_until_complete(
+                self.loop.create_server(lambda: ServerProtocol(self),
+                                        port=55555))
+            print('Serving on {}'.format(
+                self.server.sockets[0].getsockname()))
             self.loop.run_forever()
         except KeyboardInterrupt:
             pass
@@ -34,15 +46,23 @@ class Server():
         self.loop.close()
 
     def objects(self):
+        """objects -- Return positions of all tracked objects in view"""
+
         return 'objects woo'
 
     def getrobot(self):
+        """robot -- Return position of the robot"""
+
         return 'robots yay'
 
     def obstacles(self):
+        """obstacles -- Return positions of all obstacles in view"""
+
         return 'some obstacles'
 
     def setspeed(self, speed_a, speed_b):
+        """setspeed speed_a speed_b -- Set motor target speed (?-??)"""
+        
         speed_a = int(speed_a)
         speed_b = int(speed_b)
         asyncio.ensure_future(self.robot.set_speed(speed_a, speed_b),
@@ -50,6 +70,8 @@ class Server():
         return 'speed: {:d}, {:d}'.format(speed_a, speed_b)
 
     def setpower(self, power_a, power_b):
+        """setpower power_a power_b -- Directly set motor power (0-500)"""
+
         power_a = int(power_a)
         power_b = int(power_b)
         asyncio.ensure_future(self.robot.set_power(power_a, power_b),
@@ -57,21 +79,21 @@ class Server():
         return 'power: {:d}, {:d}'.format(power_a, power_b)
 
     def setparam(self, name, value):
+        """setparam name value -- Configure the robot (not implemented)"""
+
         raise NotImplementedError('Nope')
 
+    def help(self, command=None):
+        """help | ? [command] -- Display all commands or details of one"""
+        if command:
+            return self.commands[command.lower()].__doc__
+        else:
+            return '\n'.join([x.__doc__ for x in self.commands.values()])
         
 class ServerProtocol(asyncio.Protocol):
     def __init__(self, server):
         self.server = server
         self.transport = None
-        self.commands = {
-            'objects': self.server.objects,
-            'robot': self.server.getrobot,
-            'obstacles': self.server.obstacles,
-            'setspeed': self.server.setspeed,
-            'setpower': self.server.setpower,
-            'setparam': self.server.setparam,
-        }
         
     def connection_made(self, transport):
         self.transport = transport
@@ -90,17 +112,19 @@ class ServerProtocol(asyncio.Protocol):
     def data_received(self, data):
         if not data:
             return
-        print('Received: {}'.format(data.strip().decode()))
-        cmd = data.split()[0].lower().decode()
+        data = data.strip().decode()
+        print('Received: {}'.format(data))
+        cmd = data.split()[0].lower()
         args = data.split()[1:]
-        try:
-            self.write(self.commands[cmd](*args))
-        except KeyError:
-            self.write('Unkown command: {}'.format(cmd))
-        except Exception as e:
-            self.write('{} occurred while handling "{}": {}'.format(
-                type(e).__name__, data.strip().decode(), str(e)))
-            raise e
+        if cmd in self.server.commands:
+            try:
+                self.write(self.server.commands[cmd](*args))
+            except Exception as e:
+                self.write('{} occurred while handling "{}": {}'.format(
+                    type(e).__name__, data.strip().decode(), str(e)))
+                raise e
+        else:
+            self.write('Unknown command: "{}"'.format(cmd))
             
     def eof_received(self):
         return False
