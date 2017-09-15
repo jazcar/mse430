0ea96c64a2e3320c8e4e035746ef4d9ec2a442b7
Kristian Sims <kristian.sims@gmail.com>
2017-09-15 12:59:26 -0600

    Successfully replaced IQmathlib, for all that I needed it (wasn't much)

--- robot/speed.h
+++ robot/speed.h
@@ -9,8 +9,10 @@
 #define SPEED_H_
 
 // Public variables
+extern long k_p, k_i, k_d, int_cap, max_speed;
 
 // Public function prototypes
+void speed_controller_init();
 void speed_controller_tick();
 void speed_a_set_target(int);
 void speed_b_set_target(int);
