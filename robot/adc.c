5f45012965d386d66506f88567152c043e446aab
Kristian Sims <kristian.sims@gmail.com>
2017-09-15 12:59:43 -0600

    Added adc.{c,h}

---
+++ robot/adc.c
@@ -0,0 +1,12 @@
+/*
+ * adc.c
+ *
+ *  Created on: Sept 07, 2017
+ *      Author: Kristian Sims
+ */
+
+#include "adc.h"
+#include <msp430.h>
+#include "mse430.h"
+#include "pins.h"
+
---
+++ robot/adc.h
@@ -0,0 +1,11 @@
+/*
+ * adc.h
+ *
+ *  Created on: Sept 07, 2017
+ *      Author: Kristian Sims
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#endif /* ADC_H_ */
